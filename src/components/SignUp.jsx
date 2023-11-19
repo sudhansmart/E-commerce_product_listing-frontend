ef9f390980d6e6d0cf8195c743e2e69297a0403e
sudhansmart <136149970+sudhansmart@users.noreply.github.com>
2023-11-20 03:17:38 +0530

    SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,65 +1,90 @@
-import React, {useState}from 'react'
-import {Container,Form,Button, FormGroup, FormLabel, FormControl} from 'react-bootstrap'
-import '../styles/signup.css'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Form, Button, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
+import axios from 'axios';
+import '../styles/signup.css';
+
 function SignUp() {
-    const [formData,setFormData]=useState({
-        name :"",
-        email:"",
-        password:""
+    const navigate = useNavigate(); // Initialize useNavigate hook
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: ""
     });
-    const handleonchange =(e)=>{
-    
-      const{name,value} = e.target
-      setFormData({ ...formData,
-        [name] : value}
-      )
+
+    const handleOnChange = (e) => {
+        e.persist();
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
     }
-    const handlesubmit=(event)=>{
-        event.preventDefault()
-        console.log(formData)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:5175/signin/verify', formData)
+            .then(response => {
+                console.log('Registration successful:', response.data);
+                if (response.data === true) {
+                    alert('Registration Successful! Please check your registered email.');
+                    navigate('/login'); // Navigate to the login page
+                } else if (response.data === false) {
+                    alert('User already exists');
+                }
+            })
+            .catch(error => {
+                console.error('Registration failed:', error);
+            });
+
         setFormData({
-            name :"",
-            email:"",
-            password:""
+            name: "",
+            email: "",
+            password: ""
         });
     }
-  return (
-   <div className='signup'>
-      <h2 className='signup-title'>Registration Form</h2>
-        <Form onSubmit={handlesubmit}>
-            <FormGroup>
-                <FormLabel>Name </FormLabel>
-                <FormControl type='text'
-                   name='name'
-                   value={formData.name}
-                   onChange={handleonchange}
-                  required
-                   />
-            </FormGroup>
-            <FormGroup>
-                <FormLabel>Email</FormLabel>
-                <FormControl type='email'
-                   name='email'
-                   value={formData.email}
-                   onChange={handleonchange}
-                   required
-                   />
-            </FormGroup>
-            <FormGroup>
-                <FormLabel>Password </FormLabel>
-                <FormControl type='password'
-                   name='password'
-                   value={formData.password}
-                   onChange={handleonchange}
-                   required
-                   />
-            </FormGroup>
-            <Button variant='primary' type='submit'>Register</Button>
-        </Form>
-        <p>Already have an Account? <Link to={'/login'}>Login</Link></p>
-   </div>
-  )
+
+    return (
+        <div className='signup'>
+            <h2 className='signup-title'>Registration Form</h2>
+            <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                    <FormLabel>Name </FormLabel>
+                    <FormControl
+                        type='text'
+                        name='name'
+                        value={formData.name}
+                        onChange={handleOnChange}
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl
+                        type='email'
+                        name='email'
+                        value={formData.email}
+                        onChange={handleOnChange}
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Password </FormLabel>
+                    <FormControl
+                        type='password'
+                        name='password'
+                        value={formData.password}
+                        onChange={handleOnChange}
+                        required
+                    />
+                </FormGroup>
+                <Button variant='primary' type='submit'>Register</Button>
+            </Form>
+            <p>Already have an Account? <Link to={'/login'}>Login</Link></p>
+        </div>
+    );
 }
 
-export default SignUp
+export default SignUp;
